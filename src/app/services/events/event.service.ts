35c4a953c291f37680c0e4e9757a4691c1ca9e24
Zach Cowen <cowen.30@osu.edu>
2021-10-31 21:07:18 -0400

    better date handling for events

--- src/app/services/events/event.service.ts
+++ src/app/services/events/event.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Event } from 'src/app/models/event.model';
 import { environment } from 'src/environments/environment';
 
@@ -18,7 +19,9 @@ export class EventService {
 	}
 
 	getEventById(id: string): Observable<Event> {
-		return this.http.get<Event>(`${this.baseUrl}/events/${id}`)
+		return this.http.get<Event>(`${this.baseUrl}/events/${id}`).pipe(map((event: Event) => {
+			return Event.deserialize(event);
+		}));
 	}
 
 	createEvent(event: Event): Observable<Event> {
