e5962a870ba1197c5812c6142308895e7d303bc1
Zach Cowen <cowen.30@osu.edu>
2021-11-06 17:58:00 -0400

    trying different envs for build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
 	production: true,
-	serviceUrl: 'https://mud-trackr-api-test.herokuapp.com'
+	serviceUrl: 'https://api.mudtrackr.com'
 };
---
+++ src/environments/environment.test.ts
@@ -0,0 +1,4 @@
+export const environment = {
+	production: false,
+	serviceUrl: 'https://mud-trackr-api-test.herokuapp.com'
+};
